d506c144d030694f6c186af27d2a5c2f777d07b5
victornaya-dev <valfonsonaya@gmail.com>
2026-03-25 17:18:58 +0100

    add lambda.tf code for the three lambdas: model,validate cleaning,

--- terraform-deploy/infra/lambda.tf
+++ terraform-deploy/infra/lambda.tf
@@ -1,3 +1,7 @@
+# ----------------------------------------
+# Lambda Validate
+# ----------------------------------------
+
 resource "aws_lambda_function" "lambda_validate" {
   function_name = "lambda_validate"
   handler       =  "lambda_function.lambda_handler"
@@ -15,3 +19,47 @@ resource "aws_lambda_function" "lambda_validate" {
     ]
   }
 }
+
+# ----------------------------------------
+# Lambda Model
+# ----------------------------------------
+
+resource "aws_lambda_function" "lambda_model"{
+  function_name = "lambda_model"
+  handler       =  "lambda_function.lambda_handler"
+  runtime = "python3.14"
+  role = "arn:aws:iam::***REMOVED***:role/service-role/lambda_model-role-ty3ci0bb"
+  timeout      = 3
+  memory_size  = 128
+  filename      = "ignore.zip"  # valor dummy
+
+  # ignora cambios en filename/source_code_hash
+  lifecycle {
+    ignore_changes = [
+      filename,
+      source_code_hash
+    ]
+  }
+}
+
+# ----------------------------------------
+# Lambda cleaning
+# ----------------------------------------
+resource "aws_lambda_function" "lambda_cleaning" {
+  function_name = "lambda_cleaning"
+  role ="arn:aws:iam::***REMOVED***:role/service-role/lambda_cleaning-role-a8bk6mlk"
+  handler       =  "lambda_function.lambda_handler"
+  runtime = "python3.14"
+  timeout      = 3
+  memory_size  = 128
+  filename      = "ignore.zip"  # valor dummy
+
+  # ignora cambios en filename/source_code_hash
+  lifecycle {
+    ignore_changes = [
+      filename,
+      source_code_hash
+    ]
+  }
+}
+
