06e2ca329cb89b9d0dc7309bb6afadc2c9bb007b
victornaya-dev <valfonsonaya@gmail.com>
2026-03-30 18:27:00 +0200

    modify file stepfunctions.tf

--- terraform-deploy/infra/stepfunctions.tf
+++ terraform-deploy/infra/stepfunctions.tf
@@ -1,8 +1,7 @@
 
 resource "aws_sfn_state_machine" "pipeline" {
 name = "StepFunction_MultipleMeteo"
-role_arn = "arn:aws:iam::329177708113:role/service-role/StepFunctions-StepFunction_MultipleMeteo-role-jhkh1eml1"
-region = "us-east-1"
+role_arn = "arn:aws:iam::329177708113:role/service-role/StepFunctions-StepFunction_MultipleMeteo-role-wmtulcu93"
 definition = "{}" # placeholder temporal
 lifecycle {
     ignore_changes = [
