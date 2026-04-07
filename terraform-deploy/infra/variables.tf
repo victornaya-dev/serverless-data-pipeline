0ab897b01221bb855f60713d9ef2b10b2f36ac63
victornaya-dev <valfonsonaya@gmail.com>
2026-04-07 19:04:53 +0200

    add variable definition typ string for region/account_id

---
+++ terraform-deploy/infra/variables.tf
@@ -0,0 +1,9 @@
+variable "account_id" {
+  description = "AWS account ID"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
